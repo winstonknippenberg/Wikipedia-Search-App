110f095058447454e937a5e9a53d38d42188713a
= <=>
2022-05-23 15:12:36 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,46 @@
+import {
+  setSearchFocus,
+  showClearTextButton,
+  clearSearchText,
+  clearPushListener,
+} from './searchBar.js';
+import { getSearchTerm, retrieveSearchResults } from './dataFunctions.js';
+import {
+  deleteSearchResults,
+  buildSearchResults,
+  clearStatsLine,
+  setStatsLine,
+} from './searchResults.js';
+
+document.addEventListener('readystatechange', (event) => {
+  event.target.readyState === 'complete' && initApp();
+});
+
+const initApp = () => {
+  setSearchFocus();
+  const search = document.getElementById('search');
+  search.addEventListener('input', showClearTextButton);
+  const clear = document.getElementById('clear');
+  clear.addEventListener('click', clearSearchText);
+  clear.addEventListener('keydown', clearPushListener);
+  const form = document.getElementById('searchBar');
+  form.addEventListener('submit', submitTheSearch);
+};
+
+const submitTheSearch = (event) => {
+  event.preventDefault();
+  deleteSearchResults();
+  processTheSearch();
+  setSearchFocus();
+};
+
+const processTheSearch = async () => {
+  clearStatsLine();
+  const searchTerm = getSearchTerm();
+  if (searchTerm === '') return; //TODO:
+  const resultArray = await retrieveSearchResults(searchTerm);
+  if (resultArray.length) {
+    buildSearchResults(resultArray);
+  }
+  setStatsLine(resultArray.length);
+};
